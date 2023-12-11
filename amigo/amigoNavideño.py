a07eb2ae5e8843e58f8fc8af3b5a8eb05f2e28c5
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2023-12-11 11:47:30 -0500

    New game

--- amigo/amigoNavideño.py
+++ amigo/amigoNavideño.py
@@ -2,22 +2,19 @@ from reportlab.pdfgen import canvas
 from reportlab.lib import pdfencrypt
 import random
 
-amigos = ["Cecilia Santos","Paula Perucho","Abelino Perucho","Luisa Santos","Alba Lucia Santos",
-          "Karol Fabiana","Lola Santos","Toya Santos","Alejandro Santos","Cecilia Neisa","Daniel Santos",
-          "Maria Alejandra Santos","Martin Santos","Nohora Ayala","Silvia Santos","Juan Martin Santos",
-          "Isabella Cadena","Nohema Santos","Pedro Castillo","Yolanda Santos","Savi","Hagee Olaya"]
+amigos = ["Martin Santos", "Nohora Ayala", "Juan Martin Santos", "Angelica Yepes", "Silvia Santos", "Tia Rochis",
+          "Tia Alicia", "Ciro Ayala", "Cristina Mojica", "Jennifer Ayala", "David Ayala"]
 
-amigoNavidenio = {"Cecilia Santos": "", "Paula Perucho": "", "Abelino Perucho": "", "Luisa Santos": "",
-                  "Alba Lucia Santos": "", "Karol Fabiana": "", "Lola Santos": "", "Toya Santos": "",
-                  "Alejandro Santos": "", "Cecilia Neisa": "", "Daniel Santos": "", "Maria Alejandra Santos": "",
-                  "Martin Santos": "", "Nohora Ayala": "", "Silvia Santos": "", "Juan Martin Santos": "",
-                  "Isabella Cadena": "", "Nohema Santos": "", "Pedro Castillo": "", "Yolanda Santos": "",
-                  "Savi": "", "Hagee Olaya": ""}
+amigoNavidenio = {"Martin Santos": "", "Nohora Ayala": "", "Juan Martin Santos": "", "Angelica Yepes": "",
+                  "Silvia Santos": "", "Tia Rochis": "", "Tia Alicia": "", "Ciro Ayala": "", "Cristina Mojica": "",
+                  "Jennifer Ayala": "", "David Ayala": ""}
 
 contrasenias = []
 
 for amigo in amigoNavidenio:
     number = random.randint(0, len(amigos) - 1)
+    while amigo == amigos[number]:
+        number = random.randint(0, len(amigos) - 1)
     amigoNavidenio[amigo] = amigos[number]
     amigos.remove(amigos[number])
 
@@ -27,16 +24,18 @@ for amigo in amigoNavidenio:
     contrasenias.append(passwordString)
     password = pdfencrypt.StandardEncryption(passwordString)
 
-    nombre = "Files/" + amigo + ".pdf"
+    nombre =  amigo + ".pdf"
     c = canvas.Canvas(nombre,bottomup=0,encrypt=password)
     c.drawString(100, 750, formato)
     c.save()
 
 
-file = open("Files/contra.txt", "w")
+file = open("contra.txt", "w")
 
 for contrasenia in contrasenias:
     file.write(contrasenia + "\n")
 
 file.close()
 
+def calculate_number():
+    number = random.randint(0, len(amigos) - 1)
